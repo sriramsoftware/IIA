ceae52f6f9b2e64f82941a9f98b2e9f405448b39
0xFireball <0xFireball@outlook.com>
2016-12-04 16:51:49 -0800

    setup nancy with OWIN

--- KQAnalytics3/src/KQAnalytics3/Program.cs
+++ KQAnalytics3/src/KQAnalytics3/Program.cs
@@ -19,6 +19,7 @@ namespace KQAnalytics3
                 .UseKestrel()
                 .UseConfiguration(config)
                 .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
                 .Build();
             host.Run();
         }
