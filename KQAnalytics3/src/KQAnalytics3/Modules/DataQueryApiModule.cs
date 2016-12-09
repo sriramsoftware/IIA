c8bda522900089452ca47f4c8d82e099cb7e5fc0
0xFireball <0xFireball@outlook.com>
2016-12-08 20:35:03 -0800

    allow querying through api

--- KQAnalytics3/src/KQAnalytics3/Modules/DataQueryApiModule.cs
+++ KQAnalytics3/src/KQAnalytics3/Modules/DataQueryApiModule.cs
@@ -1,11 +1,25 @@
-﻿using Nancy;
+﻿using KQAnalytics3.Models.Data;
+using KQAnalytics3.Services.DataCollection;
+using Nancy;
+using Nancy.Security;
 
 namespace KQAnalytics3.Modules
 {
     public class DataQueryApiModule : NancyModule
     {
-        public DataQueryApiModule()
+        public DataQueryApiModule() : base("/api")
         {
+            // Require stateless auth
+            this.RequiresAuthentication();
+
+            // Limit is the max number of log requests to return. Default 100
+            Get("/query/{limit}", args =>
+            {
+                var itemLimit = args.limit ?? 100;
+                return Response.AsJson(
+                    (LogRequest[])DataLoggerService.QueryRequests(itemLimit).ToArray()
+                );
+            });
         }
     }
 }
