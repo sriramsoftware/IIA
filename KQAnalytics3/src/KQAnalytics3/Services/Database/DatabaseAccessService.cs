39f33ca7fc5ed1acd1221becaf42c01148e03e5d
0xFireball <0xFireball@outlook.com>
2016-12-08 19:02:39 -0800

    add database service

---
+++ KQAnalytics3/src/KQAnalytics3/Services/Database/DatabaseAccessService.cs
@@ -0,0 +1,15 @@
+﻿using LiteDB;
+
+namespace KQAnalytics3.Services.Database
+{
+    public static class DatabaseAccessService
+    {
+        public static string UsersCollectionDatabaseKey => "Users";
+
+        public static LiteDatabase OpenOrCreateDefault()
+        {
+            //kqanalytics3.lidb
+            return new LiteDatabase("kqanalytics.lidb");
+        }
+    }
+}
