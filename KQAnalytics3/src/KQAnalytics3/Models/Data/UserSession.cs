841a3f4edf64c7a69da91dd1bcd76e589b3ce9fc
0xFireball <0xFireball@outlook.com>
2016-12-09 16:54:23 -0800

    name serialization properties

--- KQAnalytics3/src/KQAnalytics3/Models/Data/UserSession.cs
+++ KQAnalytics3/src/KQAnalytics3/Models/Data/UserSession.cs
@@ -1,11 +1,14 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace KQAnalytics3.Models.Data
 {
     public class UserSession : DatabaseObject
     {
+        [JsonProperty("id")]
         public string SessionId { get; set; } = Guid.NewGuid().ToString("N");
 
+        [JsonProperty("userAgent")]
         public string UserAgent { get; set; }
     }
 }
