7742b4211e9a1b7706a54d32c812af91cc122466
0xFireball <0xFireball@outlook.com>
2016-12-13 20:16:09 -0800

    change dir to read global.json

--- build/build.sh
+++ build/build.sh
@@ -13,11 +13,19 @@ fi
 
 dotnet --version
 
+# Enter project
+
+cd KQAnalytics3
+
 # Restore packages
-dotnet restore --configfile ./build/NuGet.config
+dotnet restore --configfile ../build/NuGet.config
 
 # Build
 dotnet build -c Release KQAnalytics3/KQAnalytics3.sln
 
 # Publish
 dotnet publish -c Release KQAnalytics3/KQAnalytics3.sln
+
+# Return
+
+cd ..
