531682f350fb6aa3ca3d38cd8c6e0006ecdcd431
0xFireball <0xFireball@outlook.com>
2016-12-13 21:28:31 -0800

    add kq client script

---
+++ KQAnalytics3/src/KQAnalytics3/wwwroot/client/kq3.js
@@ -0,0 +1,24 @@
+﻿function mkuid(lnt) {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < lnt; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
+}
+
+var kqserver = _kqdaq['s'];
+var url = _kqdaq['u'];
+var trackingId = _kqdaq['tid'];
+var sessid = localStorage.getItem("sid") || mkuid(26);
+localStorage.setItem("sid", sessid);
+
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/{(\d+)}/g, function (match, number) {
+        return typeof args[number] != 'undefined' ? args[number] : match;
+    });
+};
+
+(function () {
+    // TODO
+})();
