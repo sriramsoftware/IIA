6b6abb69a7904d6aac2a1cb8215a48a616a4ccd1
0xFireball <0xFireball@outlook.com>
2016-12-15 15:30:40 -0800

    remove static content mappings

--- KQAnalytics3/src/KQAnalytics3/Bootstrapper.cs
+++ KQAnalytics3/src/KQAnalytics3/Bootstrapper.cs
@@ -19,12 +19,6 @@ namespace KQAnalytics3
         protected override void ConfigureConventions(NancyConventions nancyConventions)
         {
             base.ConfigureConventions(nancyConventions);
-            nancyConventions.StaticContentsConventions.Add(
-                StaticContentConventionBuilder.AddDirectory("assets", "wwwroot/assets/")
-            );
-            nancyConventions.StaticContentsConventions.Add(
-                StaticContentConventionBuilder.AddDirectory("static", "wwwroot/static/")
-            );
         }
 
         protected override void ApplicationStartup(TinyIoCContainer container, IPipelines pipelines)
