2e3d0c9ba24c62e66292b8293cf3c9e58ae78d8a
0xFireball <0xFireball@outlook.com>
2016-12-15 16:45:46 -0800

    add utility to query access scope

--- KQAnalytics3/src/KQAnalytics3/Services/Authentication/ClientApiAccessValidator.cs
+++ KQAnalytics3/src/KQAnalytics3/Services/Authentication/ClientApiAccessValidator.cs
@@ -7,9 +7,21 @@ namespace KQAnalytics3.Services.Authentication
 {
     public static class ClientApiAccessValidator
     {
+        public static string AuthTypeKey => "authType";
+        public static string AccessScopeKey => "accessScope";
+
         public static IEnumerable<Claim> GetAuthClaims(ApiAccessKey accessKey)
         {
-            throw new NotImplementedException();
+            return new Claim[]
+            {
+                new Claim(AuthTypeKey, "stateless"),
+                new Claim(AccessScopeKey, accessKey.AccessScope.ToString())
+            };
+        }
+
+        public static ApiAccessScope GetAccessScope(Claim accessScopeClaim)
+        {
+            return (ApiAccessScope)Enum.Parse(typeof(ApiAccessScope), accessScopeClaim.Value);
         }
     }
 }
