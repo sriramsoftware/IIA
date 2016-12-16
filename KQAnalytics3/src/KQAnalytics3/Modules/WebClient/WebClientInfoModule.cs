5b06718427d8d57868b2d0aa7cf0faeb627bfa26
0xFireball <0xFireball@outlook.com>
2016-12-15 19:57:39 -0800

    add info module

---
+++ KQAnalytics3/src/KQAnalytics3/Modules/WebClient/WebClientInfoModule.cs
@@ -0,0 +1,12 @@
+﻿using Nancy;
+
+namespace KQAnalytics3.Modules.WebClient
+{
+    public class WebClientInfoModule : NancyModule
+    {
+        public WebClientInfoModule()
+        {
+            Get("/info", _ => $"KQ Analytics");
+        }
+    }
+}
