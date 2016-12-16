ee5a52da07cee51b63d3abe366132e87d3e60de0
0xFireball <0xFireball@outlook.com>
2016-12-15 18:41:17 -0800

    code cleanup

--- KQAnalytics3/src/KQAnalytics3/Configuration/Access/ApiAccessKey.cs
+++ KQAnalytics3/src/KQAnalytics3/Configuration/Access/ApiAccessKey.cs
@@ -2,14 +2,19 @@
 
 namespace KQAnalytics3.Configuration.Access
 {
+    /// <summary>
+    /// An enumeration of access scope identifiers, used for granular permission grants on key access
+    /// </summary>
     public enum ApiAccessScope
     {
         // General
         Read,
+
         Write,
         Admin,
 
         // Types of data
+
         QueryLogRequests,
         QueryTagRequests,
         QuerySessionData,
