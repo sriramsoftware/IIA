aa7032c6c262f60cb29deeeb6c7ec5072ce7e34c
0xFireball <0xFireball@outlook.com>
2016-12-15 20:01:04 -0800

    add server info string

---
+++ KQAnalytics3/src/KQAnalytics3/Metadata/KQAnalyticsServerInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace KQAnalytics3.Metadata
+{
+    public static class KQAnalyticsServerInfo
+    {
+        public static string KQAnalyticsProductName => "KQAnalytics3 Community Edition";
+    }
+}
