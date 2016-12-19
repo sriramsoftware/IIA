2a08c8dd2ca7d8d95db0c262dadd2ef7aa7eeb33
0xFireball <0xFireball@outlook.com>
2016-12-19 13:35:51 -0800

    update kq script

--- KQAnalytics3/src/KQAnalytics3/Resources/kq3.js
+++ KQAnalytics3/src/KQAnalytics3/Resources/kq3.js
@@ -1,24 +1,27 @@
-﻿function mkuid(lnt) {
-    var text = "";
-    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-    for (var i = 0; i < lnt; i++)
-        text += possible.charAt(Math.floor(Math.random() * possible.length));
-    return text;
+﻿function mkuid (lnt) {
+  var text = ''
+  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+  for (var i = 0; i < lnt; i++) {
+    text += possible.charAt(Math.floor(Math.random() * possible.length))
+  }
+  return text
 }
 
-var kqserver = _kqdaq['s'];
-var url = _kqdaq['u'];
-var trackingId = _kqdaq['tid'];
-var sessid = localStorage.getItem("sid") || mkuid(26);
-localStorage.setItem("sid", sessid);
+var kc = window._kqdaq || {}
+var kqserver = kc['s']
+if (kqserver.substr(-1) != '/') kqserver += '/'
+var url = kc['u']
+var trackingId = kc['tid']
+var sid = window.localStorage.getItem('sid') || mkuid(26)
+window.localStorage.setItem('sid', sid)
 
 String.prototype.format = function () {
-    var args = arguments;
-    return this.replace(/{(\d+)}/g, function (match, number) {
-        return typeof args[number] != 'undefined' ? args[number] : match;
-    });
+  var args = arguments
+  return this.replace(/{(\d+)}/g, function (match, number) {
+    return typeof args[number] !== 'undefined' ? args[number] : match
+  })
 };
 
 (function () {
-    // TODO
-})();
+  // TODO
+})()
