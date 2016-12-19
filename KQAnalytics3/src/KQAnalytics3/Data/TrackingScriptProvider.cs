2dabff04da2dd798c0c0eaca68f158879d429f31
0xFireball <0xFireball@outlook.com>
2016-12-19 13:10:26 -0800

    fetch kq3.js script from resource service

--- KQAnalytics3/src/KQAnalytics3/Data/TrackingScriptProvider.cs
+++ KQAnalytics3/src/KQAnalytics3/Data/TrackingScriptProvider.cs
@@ -1,13 +1,15 @@
-﻿using System;
+﻿using KQAnalytics3.Services.Resources;
 using System.IO;
 
 namespace KQAnalytics3.Data
 {
     public static class TrackingScriptProvider
     {
+        public static string TrackingScriptName => "kq3.js";
+
         public static Stream CreateTrackingScript()
         {
-            throw new NotImplementedException();
+            return ResourceProviderService.GetResource(TrackingScriptName);
         }
     }
 }
