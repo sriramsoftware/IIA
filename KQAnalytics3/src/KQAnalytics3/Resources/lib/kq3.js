2f8254efe6e0c7841499ef9c2d81b7716c7f5585
0xFireball <0xFireball@outlook.com>
2016-12-19 14:00:26 -0800

    fix babel build

--- KQAnalytics3/src/KQAnalytics3/Resources/lib/kq3.js
+++ KQAnalytics3/src/KQAnalytics3/Resources/lib/kq3.js
@@ -1,27 +1,2 @@
-function mkuid(lnt) {
-  var text = '';
-  var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-  for (var i = 0; i < lnt; i++) {
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  }
-  return text;
-}
-
-var kc = window._kqdaq || {};
-var kqserver = kc['s'];
-if (kqserver.substr(-1) != '/') kqserver += '/';
-var url = kc['u'];
-var trackingId = kc['tid'];
-var sid = window.localStorage.getItem('sid') || mkuid(26);
-window.localStorage.setItem('sid', sid);
-
-String.prototype.format = function () {
-  var args = arguments;
-  return this.replace(/{(\d+)}/g, function (match, number) {
-    return typeof args[number] !== 'undefined' ? args[number] : match;
-  });
-};
-
-(function () {
-  // TODO
-})();
+'use strict';function mkuid(a){var b='',c='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';for(var d=0;d<a;d++)b+=c.charAt(Math.floor(Math.random()*c.length));return b}var kc=window._kqdaq||{},kqserver=kc.s;'/'!=kqserver.substr(-1)&&(kqserver+='/');var url=kc.u,trackingId=kc.tid,sid=window.localStorage.getItem('sid')||mkuid(26);window.localStorage.setItem('sid',sid),String.prototype.format=function(){var a=arguments;return this.replace(/{(\d+)}/g,function(b,c){return'undefined'==typeof a[c]?b:a[c]})},function(){// TODO
+}();
