4143784153d696f43587424def5733b2177e9114
0xFireball <0xFireball@outlook.com>
2016-12-19 12:39:56 -0800

    add service for accessing resources

---
+++ KQAnalytics3/src/KQAnalytics3/Services/Resources/ResourceProviderService.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+
+namespace KQAnalytics3.Services.Resources
+{
+    public static class ResourceProviderService
+    {
+        public static string ResourceBasePath => Path.Combine("Resources");
+
+        public static Stream GetResource(string resourcePath)
+        {
+            var resCompletePath = Path.Combine(ResourceBasePath, resourcePath);
+            return File.OpenRead(resourcePath);
+        }
+
+        public static string ReadResourceContents(string resourcePath)
+        {
+            var result = string.Empty;
+            using (var sr =new StreamReader(GetResource(resourcePath)))
+            {
+                result = sr.ReadToEnd();
+            }
+            return result;
+        }
+    }
+}
