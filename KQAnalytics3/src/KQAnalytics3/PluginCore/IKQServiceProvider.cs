4d806947358c85af92b22716b34140d5a8ad6727
0xFireball <0xFireball@outlook.com>
2016-12-25 13:23:32 -0800

    provide internal service access to components

--- KQAnalytics3/src/KQAnalytics3/PluginCore/IKQServiceProvider.cs
+++ KQAnalytics3/src/KQAnalytics3/PluginCore/IKQServiceProvider.cs
@@ -1,6 +1,14 @@
-﻿namespace KQAnalytics3.PluginCore
+﻿using KQAnalytics3.Services.DataCollection;
+using KQAnalytics3.Services.DataQuery;
+using KQAnalytics3.Services.Resources;
+
+namespace KQAnalytics3.PluginCore
 {
     public interface IKQServiceProvider
     {
+        DataLoggerService DataLoggerService { get; }
+        SessionStorageService SessionStorageService { get; }
+        DataQueryDateService DataQueryDateService { get; }
+        ResourceProviderService ResourceProviderService { get; }
     }
 }
