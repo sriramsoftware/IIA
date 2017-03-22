3ff2125498a10100c3c7f28e9b54f028afb06ba7
0xFireball <0xFireball@outlook.com>
2017-03-21 17:57:21 -0700

    temporarily disable whitelist

--- src/IridiumIon.Analytics/NABootstrapper.cs
+++ src/IridiumIon.Analytics/NABootstrapper.cs
@@ -3,13 +3,11 @@ using IridiumIon.Analytics.Configuration;
 using IridiumIon.Analytics.Models.Data;
 using IridiumIon.Analytics.PluginCore;
 using IridiumIon.Analytics.Services.Authentication;
-using IridiumIon.Analytics.Utilities;
 using Nancy;
 using Nancy.Authentication.Stateless;
 using Nancy.Bootstrapper;
 using Nancy.Session;
 using Nancy.TinyIoc;
-using System.Linq;
 
 namespace IridiumIon.Analytics
 {
@@ -41,59 +39,6 @@ namespace IridiumIon.Analytics
                 // get user identity
                 return ClientAuthenticationService.ResolveClientIdentity(apiKey);
             }));
-
-            // Enable CORS
-            pipelines.AfterRequest.AddItemToEndOfPipeline((ctx) =>
-            {
-                if (KQRegistry.ServerConfiguration.CorsOptions != null)
-                {
-                    foreach (var origin in KQRegistry.ServerConfiguration.CorsOptions.Origins)
-                    {
-                        ctx.Response.WithHeader("Access-Control-Allow-Origin", origin);
-                    }
-                    ctx.Response
-                        .WithHeader("Access-Control-Allow-Methods", "POST,GET")
-                        .WithHeader("Access-Control-Allow-Headers", "Accept, Origin, Content-type");
-                }
-            });
-
-            // Set up whitelist/blacklist
-            if (KQRegistry.ServerConfiguration.BlacklistConfiguration != null
-                && KQRegistry.ServerConfiguration.BlacklistConfiguration.Enable
-                && KQRegistry.ServerConfiguration.WhitelistConfiguration != null
-                && KQRegistry.ServerConfiguration.WhitelistConfiguration.Enable)
-            {
-                throw new ConfigurationException("The whitelist and the blacklist cannot be enabled simultaneously");
-            }
-
-            if (KQRegistry.ServerConfiguration.BlacklistConfiguration != null
-                && KQRegistry.ServerConfiguration.BlacklistConfiguration.Enable)
-            {
-                pipelines.BeforeRequest.AddItemToStartOfPipeline((ctx) =>
-                {
-                    var userAddr = ctx.Request.UserHostAddress;
-                    if (KQRegistry.ServerConfiguration.BlacklistConfiguration.Hosts.Any(blacklistedAddress => WildcardMatcher.IsMatch(userAddr, blacklistedAddress)))
-                    {
-                        return new Response().WithStatusCode(HttpStatusCode.Forbidden);
-                    }
-                    return null; // Take no action
-                });
-            }
-
-            if (KQRegistry.ServerConfiguration.WhitelistConfiguration != null
-                && KQRegistry.ServerConfiguration.WhitelistConfiguration.Enable)
-            {
-                pipelines.BeforeRequest.AddItemToStartOfPipeline((ctx) =>
-                {
-                    var userAddr = ctx.Request.UserHostAddress;
-                    if (KQRegistry.ServerConfiguration.BlacklistConfiguration.Hosts.Any(whitelistedAddress => WildcardMatcher.IsMatch(userAddr, whitelistedAddress)))
-                    {
-                        return null; // Pass it on
-                    }
-                    // Not on whitelist
-                    return new Response().WithStatusCode(HttpStatusCode.Forbidden);
-                });
-            }
 
             // Initialize object data mapper
             Mapper.Initialize(cfg =>
